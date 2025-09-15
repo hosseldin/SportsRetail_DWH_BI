9f4ff7b4071524f0b0137de0a29331e1aa1c1630
Hossam Mahmoud <hossam.uddin1@gmail.com>
2025-09-15 03:14:37 +0300

    editing the first script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,34 +1,34 @@
-/*
-=============================================================
-Create Database and Schemas
-=============================================================
+
+/*==============================================================
+ Author		: Hossam Mahmoud
+ Purpose	: Create Database and Schemas
+ Date			: 2025-08-11
+ Notes		: 
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
+	This script creates a new database named 'SR_DWH_DB' after checking if it already exists. 
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+	within the database: 'bronze', 'silver', and 'gold'.
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
-*/
+	Running this script will drop the entire 'SR_DWH_DB' database if it exists. 
+	All data in the database will be permanently deleted. Proceed with caution 
+	and ensure you have proper backups before running this script.
+==============================================================*/
 
+-- Switch context to the 'master' database
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
+-- Drop and recreate the 'SR_DWH_DB' database
+ALTER DATABASE SR_DWH_DB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE IF EXISTS SR_DWH_DB;
 GO
 
--- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
+-- Create the 'SR_DWH_DB' database
+CREATE DATABASE SR_DWH_DB;
 GO
 
-USE DataWarehouse;
+USE SR_DWH_DB;
 GO
 
 -- Create Schemas
